75444de16bb271a521c00f5931ed371cc718bf0c
aimy <kawamoto.takahisa@gmail.com>
2017-07-17 17:40:20 +0900

    library added (List, Math)

---
+++ List.hs
@@ -0,0 +1,17 @@
+module List (
+    splitOn
+  , comb
+) where
+
+import Control.Monad
+import Data.List
+
+splitOn :: Int -> [a] -> [[a]]
+splitOn n = unfoldr (\xs -> guard ((not.null) xs) >> return (splitAt n xs))
+
+comb :: Int -> [a] -> [[a]]
+comb n xs = go n xs [] []
+  where
+    go 0 _ ys zs = reverse ys : zs
+    go _ [] _  zs = zs
+    go m (w:ws) ys zs = go (m-1) ws (w:ys) (go m ws ys zs)
---
+++ Math.hs
@@ -0,0 +1,22 @@
+module Math (
+    primes
+  , factorization
+) where
+
+import Data.List
+
+primes :: [Int]
+primes = 2 : 3 : [x | i<-[1..], j<-[-1,1], let x = 6*i+j, isPrime x]
+  where isPrime n = null [i | i <- takeWhile (\p -> p*p <= n) primes, mod n i == 0]
+
+factorization :: Int -> [Int]
+factorization n = unfoldr go (n, primes)
+  where
+    go (_, []) = Nothing
+    go (m, pps@(p:ps))
+      | m == 1 = Nothing
+      | p > isqrt n = Just (m, (1, pps))
+      | otherwise = let (q,r) = divMod m p in if r == 0 then Just (p, (q,pps)) else go (m,ps)
+
+isqrt :: Integral a => a -> a
+isqrt = floor . sqrt . (fromIntegral :: Integral a => a -> Double)
