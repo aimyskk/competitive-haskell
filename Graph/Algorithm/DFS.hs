d6c27c188c129506b6ddd3c11061adbd14472641
aimy <kawamoto.takahisa@gmail.com>
2017-07-21 23:49:23 +0900

    Bugfix

--- Graph/Algorithm/DFS.hs
+++ Graph/Algorithm/DFS.hs
@@ -5,16 +5,21 @@ module Graph.Algorithm.DFS (
 import Graph.Graph
 
 import qualified Data.IntSet as S
-import Data.Monoid
 
-dfs :: Monoid m => Graph -> (Vertexes -> m) -> Vertex -> m
-dfs g p v = dfsCore g p v visited mempty
+-- modify according to your purpose
+type Acc = Int
+
+dfs :: Graph -> (Vertex -> Acc -> Acc) -> Vertex -> Acc
+dfs g p v = fst $ dfsCore g p v (1, visited)
   where
-    visited = S.empty
+   visited = S.singleton v
 
-dfsCore :: Monoid m => Graph -> (Vertexes -> m) -> Vertex -> Vertexes -> m -> m
-dfsCore g p v visited m
-  | S.null next = p visited <> m
-  | otherwise = S.foldr (\w acc -> acc <> dfsCore g p w (S.insert v visited) m) mempty next
+dfsCore :: Graph -> (Vertex -> Acc -> Acc) -> Vertex -> (Acc, Vertexes) -> (Acc, Vertexes)
+dfsCore g p v (m, visited)
+  | S.null next = (m, visited)
+  | otherwise = S.foldr (branch g p) (m, visited) next
   where
     next = S.difference (from g v) visited
+
+branch :: Graph -> (Vertex -> Acc -> Acc) -> Vertex -> (Acc, Vertexes) -> (Acc, Vertexes)
+branch g p w (aa,avd) = dfsCore g p w (p w aa, S.insert w avd)
