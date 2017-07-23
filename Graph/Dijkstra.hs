ea16008fa43b1b79a25c190e9b879e0aab8908cb
aimy <kawamoto.takahisa@gmail.com>
2017-07-23 14:42:59 +0900

    modulo combination added

--- Graph/Dijkstra.hs
+++ Graph/Dijkstra.hs
@@ -3,14 +3,12 @@ module Graph.Dijkstra (
 ) where
 
 import WeightedGraph
-import Heap.LeftistHeap
 
 import qualified Data.Set as S
 import qualified Data.IntMap as M
 
 type Path = [Vertex]
 type Memo = M.IntMap (Weight, Path)
-type PriorityQueue = LeftistHeap (Weight, Vertex)
 
 -- accumrated weight and lexical order shortest path
 dijkstra :: Graph -> Vertex -> Memo
@@ -29,3 +27,17 @@ dijkstraCore g q m
     m1 = S.foldr (\(t,w) acc -> M.insertWith min t (w0+w,p) acc) m vns
     q2 = S.foldr (\(t,w) acc -> insert (w0+w, t) acc) q1 vns
     vns = S.filter (\(t,w) -> M.notMember t m || w0+w <= fst (m M.! t)) (from g s)
+
+type PriorityQueue = S.Set (Weight, Vertex)
+
+isEmpty :: PriorityQueue -> Bool
+isEmpty = S.null
+
+singleton :: (Weight, Vertex) -> PriorityQueue
+singleton = S.singleton
+
+insert :: (Weight, Vertex) -> PriorityQueue -> PriorityQueue
+insert = S.insert
+
+deleteFindMin :: PriorityQueue -> ((Weight, Vertex), PriorityQueue)
+deleteFindMin = S.deleteFindMin
--- Math.hs
+++ Math.hs
@@ -1,17 +1,31 @@
 module Math (
   primes,
   factorization,
-  isqrt
+  isqrt,
+  comb,
+  fact,
+  pow,
+  combMod,
+  factTable
 ) where
 
 import Data.List
+import qualified Data.Array.IArray as A
 
-primes :: [Int]
+type FactTable = A.Array Int (Int,Int)
+
+modulo :: Int
+modulo = 1000000007
+
+(*%) :: Int -> Int -> Int
+(*%) x y = mod (x*y) modulo
+
+primes :: Integral a => [a]
 primes = 2 : 3 : [x | i <- [1..], j <- [-1,1], let x = 6*i+j, isPrime x]
   where
     isPrime n = null [i | i <- takeWhile (\p -> p*p <= n) primes, mod n i == 0]
 
-factorization :: Int -> [Int]
+factorization :: Integral a => a -> [a]
 factorization n = unfoldr go (n, primes)
   where
     go (_, []) = Nothing
@@ -22,3 +36,26 @@ factorization n = unfoldr go (n, primes)
 
 isqrt :: Integral a => a -> a
 isqrt = floor . sqrt . (fromIntegral :: Integral a => a -> Double)
+
+comb :: Integral a => a -> a -> a
+comb n r = div (fact n) (fact r * fact (n-r))
+
+fact :: Integral a => a -> a
+fact n = product [1..n]
+
+pow :: Int -> Int -> Int
+pow x n
+  | n == 0 = 1
+  | odd n = x *% pow x (n-1)
+  | otherwise = mod (pow x (div n 2) ^ (2 :: Int)) modulo
+
+combMod :: FactTable -> Int -> Int -> Int
+combMod t n r
+  | n < r = 0
+  | otherwise = fst (t A.! n) *% snd (t A.! r) *% snd (t A.! (n-r))
+
+factTable :: Int -> FactTable
+factTable n = A.listArray (0, n) (zip facts factInvs)
+  where
+    facts = scanl (*%) 1 [1..n]
+    factInvs = map (\x -> mod (pow x (modulo - 2)) modulo) facts
