f6613a2b65e470321b7ca002692d8aa55f4446de
aimy <kawamoto.takahisa@gmail.com>
2017-08-05 15:27:17 +0900

    Refactoring

--- Math.hs
+++ Math.hs
@@ -14,26 +14,23 @@ module Math (
 import Data.List
 import qualified Data.Array.IArray as A
 
-type FactTable = A.Array Int (Int,Int)
+type FactTable = A.Array Int (Integer, Integer)
 
-modulus :: Int
+modulus :: Integer
 modulus = 1000000007
 
-(+%) :: Int -> Int -> Int
+(+%) :: Integer -> Integer -> Integer
 (+%) x y = mod (x + y) modulus
 
-(*%) :: Int -> Int -> Int
-(*%) x y = fromIntegral $ mod (xz * yz) (fromIntegral modulus)
-  where
-    xz = fromIntegral x :: Integer
-    yz = fromIntegral y :: Integer
+(*%) :: Integer -> Integer -> Integer
+(*%) x y = mod (x * y) modulus
 
-primes :: Integral t => [t]
+primes :: Integral a => [a]
 primes = 2 : 3 : [x | i <- [1..], j <- [-1,1], let x = 6*i+j, isPrime x]
   where
     isPrime n = null [i | i <- takeWhile (\p -> p*p <= n) primes, mod n i == 0]
 
-factorization :: Integral t => t -> [t]
+factorization :: Integral a => a -> [a]
 factorization n = unfoldr go (n, primes)
   where
     go (_, []) = Nothing
@@ -42,8 +39,8 @@ factorization n = unfoldr go (n, primes)
       | p > isqrt n = Just (m, (1, pps))
       | otherwise = let (q,r) = divMod m p in if r == 0 then Just (p, (q,pps)) else go (m,ps)
 
-isqrt :: Integral t => t -> t
-isqrt = floor . sqrt . (fromIntegral :: Integral a => a -> Double)
+isqrt :: Integral a => a -> a
+isqrt = floor . sqrt . (fromIntegral :: Integral t => t -> Double)
 
 comb :: Integral a => a -> a -> a
 comb n r = div (fact n) (fact r * fact (n - r))
@@ -51,19 +48,22 @@ comb n r = div (fact n) (fact r * fact (n - r))
 fact :: Integral a => a -> a
 fact n = product [1 .. n]
 
-pow :: Int -> Int -> Int
+pow :: Integer -> Integer -> Integer
 pow x n
   | n == 0 = 1
   | odd n = x *% pow x (n-1)
   | otherwise = let y = pow x (div n 2) in y *% y
 
-combMod :: FactTable -> Int -> Int -> Int
+combMod :: FactTable -> Integer -> Integer -> Integer
 combMod t n r
   | n < r = 0
-  | otherwise = fst (t A.! n) *% snd (t A.! r) *% snd (t A.! (n-r))
+  | otherwise = fst (t ! n) *% snd (t ! r) *% snd (t ! (n - r))
 
 factTable :: Int -> FactTable
 factTable n = A.listArray (0, n) (zip facts factInvs)
   where
-    facts = scanl (*%) 1 [1..n]
+    facts = scanl (*%) 1 [1 .. fromIntegral n]
     factInvs = map (\x -> mod (pow x (modulus - 2)) modulus) facts
+
+(!) :: FactTable -> Integer -> (Integer, Integer)
+(!) t i = t A.! fromIntegral i
