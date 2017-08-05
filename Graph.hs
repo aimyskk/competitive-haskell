d525ae15d7b66ba474db280ea76ed1b8f0aa4b49
aimy <kawamoto.takahisa@gmail.com>
2017-08-05 10:39:30 +0900

    Graph.readBitmap added

--- Graph.hs
+++ Graph.hs
@@ -8,7 +8,8 @@ module Graph (
   size,
   from,
   readUndirectedEdge,
-  readDirectedEdge
+  readDirectedEdge,
+  readBitmap
 ) where
 
 import qualified Data.ByteString.Char8 as B
@@ -17,11 +18,16 @@ import qualified Data.Array.IArray as A
 
 import Scanner (readInt)
 
+--type Height = Int
+type Width = Int
+type Index = Int
+
 type Vertex = Int
 type Vertexes = S.IntSet
 type Edge = (Vertex, Vertex)
 type Graph = A.Array Vertex Vertexes
 
+-- 1-indexed
 buildG :: Int -> [Edge] -> Graph
 buildG n = A.accumArray (flip S.insert) S.empty (1, n)
 
@@ -44,3 +50,13 @@ constructOneWay _ = undefined
 constructTwoWay :: [Int] -> [Edge]
 constructTwoWay [s,t] = [(s, t), (t, s)]
 constructTwoWay _ = undefined
+
+readBitmap :: Width -> B.ByteString -> [Edge]
+readBitmap w bs = foldl (scout w bs) [] [0 .. B.length bs]
+
+scout :: Width -> B.ByteString -> [Edge] -> Index -> [Edge]
+scout w bs acc i = zip (repeat i) neighbor ++ acc
+  where
+    neighbor = filter (\x -> inner x && movable x) [i-1, i+1, i-w, i+w]
+    inner x = x >= 0 && x < B.length bs
+    movable x = B.index bs x `elem` ".sg"
