5517d44db0de8ced64de4998a20563f61e17b5bd
aimy <kawamoto.takahisa@gmail.com>
2017-08-06 11:53:06 +0900

    Performance improvement

--- Tree/UnionFind.hs
+++ Tree/UnionFind.hs
@@ -1,12 +1,13 @@
 module UnionFind (
   UnionFind,
+
+  initial,
+  (!?),
   rep,
   rank,
+  isolate,
   merge,
-  same,
-  fromList,
-  fromAscList,
-  fromDistinctAscList
+  same
 ) where
 
 import qualified Data.IntMap as M
@@ -17,44 +18,38 @@ data UnionFind = UnionFind {tree :: M.IntMap Point, rk :: M.IntMap Rank}
 type Point = Int
 type Rank = Int
 
+initial :: UnionFind
+initial = UnionFind (M.singleton 0 0) M.empty
+
+(!?) :: UnionFind -> Point -> Point
+(!?) uf p = M.findWithDefault 0 p (tree uf)
+
 rep :: UnionFind -> Point -> Point
-rep uf = untilFix (tree uf M.!)
+rep uf = untilFix (uf !?)
 
 rank :: UnionFind -> Point -> Rank
-rank uf = (M.!) (rk uf)
+rank uf = (rk uf M.!)
+
+isolate :: UnionFind -> Point -> Bool
+isolate uf p = rep uf p == 0
 
 merge :: UnionFind -> Point -> Point -> UnionFind
 merge uf p1 p2
-  | rp1 /= rp2 && rk1 > rk2 = UnionFind tree12 rank1
-  | rp1 /= rp2 = UnionFind tree21 rank2
+  | i1 && i2 = UnionFind (M.insert p2 p1 (M.insert p1 p1 (tree uf))) (M.insert p1 2 (rk uf))
+  | i1 = UnionFind (M.insert p1 rp2 (tree uf)) (M.insert rp2 (rk2 + 1) (rk uf))
+  | i2 = UnionFind (M.insert p2 rp1 (tree uf)) (M.insert rp1 (rk1 + 1) (rk uf))
+  | rp1 /= rp2 && rk1 > rk2 = UnionFind (M.insert p2 rp1 (M.insert rp2 rp1 (tree uf))) (M.insert rp1 (rk1 + rk2) (rk uf))
+  | rp1 /= rp2 = UnionFind (M.insert p1 rp2 (M.insert rp1 rp2 (tree uf))) (M.insert rp2 (rk1 + rk2) (rk uf))
   | otherwise = uf
   where
-    tree12 = M.insert p2 rp1 (M.insert rp2 rp1 (tree uf))
-    tree21 = M.insert p1 rp2 (M.insert rp1 rp2 (tree uf))
-    rank1 = M.insert rp1 (rk1 + rk2) (rk uf)
-    rank2 = M.insert rp2 (rk1 + rk2) (rk uf)
+    i1 = isolate uf p1
+    i2 = isolate uf p2
     rk1 = rank uf rp1
     rk2 = rank uf rp2
     rp1 = rep uf p1
     rp2 = rep uf p2
 
 same :: UnionFind -> Point -> Point -> Bool
-same uf p1 p2 = rep uf p1 == rep uf p2
-
-fromList :: [Point] -> UnionFind
-fromList ps = UnionFind tree0 rk0
-  where
-    tree0 = M.fromList [(i, i) | i <- ps]
-    rk0 = M.fromList [(i, 1) | i <- ps]
-
-fromAscList :: [Point] -> UnionFind
-fromAscList ps = UnionFind tree0 rk0
-  where
-    tree0 = M.fromAscList [(i, i) | i <- ps]
-    rk0 = M.fromAscList [(i, 1) | i <- ps]
-
-fromDistinctAscList :: [Point] -> UnionFind
-fromDistinctAscList ps = UnionFind tree0 rk0
-  where
-    tree0 = M.fromDistinctAscList [(i, i) | i <- ps]
-    rk0 = M.fromDistinctAscList [(i, 1) | i <- ps]
+same uf p1 p2
+  | p1 == p2 = True
+  | otherwise = not (isolate uf p1) && (rep uf p1 == rep uf p2)
