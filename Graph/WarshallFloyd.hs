6429a79f1c7947441a95dd1a8dd5826c49e3f648
aimy <kawamoto.takahisa@gmail.com>
2017-08-16 00:41:30 +0900

    Simplify

--- Graph/WarshallFloyd.hs
+++ Graph/WarshallFloyd.hs
@@ -6,23 +6,22 @@ import qualified Data.Map.Strict as M
 
 import WeightedGraph
 
-type Memo = M.Map (Vertex, Vertex) (Weight, Path)
+type Memo = M.Map (Vertex, Vertex) Weight
 
--- accumrated weight and lexical order shortest path
 -- 0-indexed vertex
 warshallFloyd :: Int -> [Edge] -> Memo
 warshallFloyd n es = foldl shorten m0 kij
   where
-    m0 = M.fromList (map (\((s, t), w) -> ((s, t), (w, [s, t]))) es)
+    m0 = M.fromList es
     kij = [(k, i, j) | k <- [0 .. pred n], i <- [0 .. pred n], j <- [0 .. pred n]]
 
 shorten :: Memo -> (Vertex, Vertex, Vertex) -> Memo
 shorten m (k, i, j) = case connect m k i j of
   Nothing -> m
-  Just (w, p) -> M.insertWith min (i, j) (w, p) m
+  Just w -> M.insertWith min (i, j) w m
 
-connect :: Memo -> Vertex -> Vertex -> Vertex -> Maybe (Weight, Path)
+connect :: Memo -> Vertex -> Vertex -> Vertex -> Maybe Weight
 connect m k i j = do
-  (w1, p1) <- M.lookup (i, k) m
-  (w2, p2) <- M.lookup (k, j) m
-  return (w1 + w2, p1 ++ tail p2)
+  w1 <- M.lookup (i, k) m
+  w2 <- M.lookup (k, j) m
+  return (w1 + w2)
