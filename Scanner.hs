ad8a7f09509884c06ba67ee8ae4b37730e006244
aimy <kawamoto.takahisa@gmail.com>
2017-08-15 23:59:59 +0900

    Refactoring

--- Scanner.hs
+++ Scanner.hs
@@ -12,17 +12,17 @@ import qualified Data.ByteString.Char8 as B
 readInt :: B.ByteString -> Int
 readInt = maybe undefined fst . B.readInt
 
-readInteger :: B.ByteString -> Integer
-readInteger = maybe undefined fst . B.readInteger
-
 readInts :: B.ByteString -> [Int]
 readInts = map readInt . B.words
 
-readIntegers :: B.ByteString -> [Integer]
-readIntegers = map readInteger . B.words
-
 readIntPair :: B.ByteString -> [(Int, Int)]
 readIntPair = map (pair . map readInt . B.words) . B.lines
+
+readInteger :: B.ByteString -> Integer
+readInteger = maybe undefined fst . B.readInteger
+
+readIntegers :: B.ByteString -> [Integer]
+readIntegers = map readInteger . B.words
 
 readIntegerPair :: B.ByteString -> [(Integer, Integer)]
 readIntegerPair = map (pair . map readInteger . B.words) . B.lines
