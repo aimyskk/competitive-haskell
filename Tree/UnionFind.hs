b7b7be69db3051cd91d2ed57080fc15664bf3135
aimy <kawamoto.takahisa@gmail.com>
2017-08-24 22:53:40 +0900

    Performance tuning

--- Tree/UnionFind.hs
+++ Tree/UnionFind.hs
@@ -8,45 +8,47 @@ module Tree.UnionFind (
 
 import qualified Data.IntMap.Strict as M
 
-import Function (untilFix)
+data UnionFind = UnionFind {
+  tree :: M.IntMap Point,
+  rk :: M.IntMap Rank
+} deriving (Eq, Show)
 
-data UnionFind = UnionFind {tree :: M.IntMap Point, rk :: M.IntMap Rank}
-type Point = Int  -- greater than 0
+type Point = Int -- greater than 0
 type Rank = Int
 
 initial :: UnionFind
-initial = UnionFind (M.singleton 0 0) M.empty
-
-(!?) :: UnionFind -> Point -> Point
-(!?) uf p = M.findWithDefault 0 p (tree uf)
+initial = UnionFind M.empty M.empty
 
 rep :: UnionFind -> Point -> Point
-rep uf = untilFix (uf !?)
+rep uf p
+  | M.notMember p (tree uf) = p
+  | tree uf M.! p == p = p
+  | otherwise = rep uf (tree uf M.! p)
 
 rank :: UnionFind -> Point -> Rank
-rank uf = (rk uf M.!)
-
-isolate :: UnionFind -> Point -> Bool
-isolate uf p = rep uf p == 0
+rank uf p = M.findWithDefault 1 p (rk uf)
 
 merge :: UnionFind -> Point -> Point -> UnionFind
-merge uf p1 p2
-  | i1 && i2 = UnionFind (M.insert p2 p1 (M.insert p1 p1 (tree uf))) (M.insert p1 2 (rk uf))
-  | i1 = UnionFind (M.insert p1 rp2 (tree uf)) (M.insert rp2 (rk2 + 1) (rk uf))
-  | i2 = UnionFind (M.insert p2 rp1 (tree uf)) (M.insert rp1 (rk1 + 1) (rk uf))
-  | rp1 /= rp2 && rk1 > rk2 = UnionFind (M.insert p2 rp1 (M.insert rp2 rp1 (tree uf))) (M.insert rp1 (rk1 + rk2) (rk uf))
-  | rp1 /= rp2 = UnionFind (M.insert p1 rp2 (M.insert rp1 rp2 (tree uf))) (M.insert rp2 (rk1 + rk2) (rk uf))
-  | otherwise = uf
+merge uf p q
+  | rp1 == rp2 = uf
+  | otherwise = UnionFind (M.insert rp2 rp1 (M.insert p2 rp1 (tree uf))) (M.insert rp1 (rk1 + rk2) (rk uf))
   where
-    i1 = isolate uf p1
-    i2 = isolate uf p2
-    rk1 = rank uf rp1
-    rk2 = rank uf rp2
+    p1 = ordBy uf p q
+    p2 = if p1 == p then q else p
     rp1 = rep uf p1
     rp2 = rep uf p2
+    rk1 = rank uf rp1
+    rk2 = rank uf rp2
 
 same :: UnionFind -> Point -> Point -> Bool
-same uf p1 p2
-  | p1 == p2 = True
-  | isolate uf p1 = False
-  | otherwise = rep uf p1 == rep uf p2
+same uf p1 p2 = rep uf p1 == rep uf p2
+
+ordBy :: UnionFind -> Point -> Point -> Point
+ordBy uf x y
+  | rx > ry = x
+  | rx < ry = y
+  | rep uf x < rep uf y = x
+  | otherwise = y
+  where
+    rx = rank uf x
+    ry = rank uf y
