fc7793ab4daacf77a816deab311f5e3c03075c9e
aimy <kawamoto.takahisa@gmail.com>
2017-09-01 22:31:27 +0900

    Replace UArray with IArray

--- Array/Cumulative.hs
+++ Array/Cumulative.hs
@@ -1,34 +1,42 @@
 module Array.Cumulative where
 
 import Data.List
-import qualified Data.Array.IArray as A
+import qualified Data.Array.Unboxed as A
 
 type Height = Int
 type Width = Int
+type Index = Int
 type Coord = (Height, Width)
-
+ 
 -- 1-indexed
-type Matrix a = A.Array Height (A.Array Width a)
-
-fromList :: Num a => Height -> Width -> [[a]] -> Matrix a
-fromList h w = A.listArray (1, h) . map (A.listArray (1, w))
-
-fromListAccum :: Num a => Height -> Width -> [[a]] -> Matrix a
-fromListAccum h w = A.listArray (1, h) . map (A.listArray (1, w)) . csum2
-
+data IMatrix = M {
+  _height :: Int,
+  _width :: Int,
+  _matrix :: A.UArray Index Int
+} deriving (Eq, Show)
+ 
+(!) :: IMatrix -> Coord -> Int
+(!) m c = _matrix m A.! idx (_width m) c
+ 
+idx :: Width -> Coord -> Index
+idx w (x, y) = (x - 1) * w + y
+ 
+fromList :: Height -> Width -> [[Int]] -> IMatrix
+fromList h w = M h w . A.listArray (1, h*w) . concat
+ 
+fromListAccum :: Height -> Width -> [[Int]] -> IMatrix
+fromListAccum h w = fromList h w . csum2
+ 
 csum1 :: Num a => [a] -> [a]
 csum1 = scanl1 (+)
-
+ 
 csum2 :: Num a => [[a]] -> [[a]]
 csum2 = transpose . map csum1 . transpose . map csum1
-
-(!) :: Matrix a -> Coord -> a
-(!) m (x, y) = (m A.! x) A.! y
-
-rsum :: Num a => Matrix a -> Coord -> Coord -> a
+ 
+rsum :: IMatrix -> Coord -> Coord -> Int
 rsum m (x1,y1) c2@(x2,y2) = a1 - a2 - a3 + a4
   where
     a1 = m ! c2
-    a2 = if x1 == 1 then 0 else m ! (x1-1, y2)
-    a3 = if y1 == 1 then 0 else m ! (x2, y1-1)
-    a4 = if x1 == 1 || y1 == 1 then 0 else m ! (x1-1, y1-1)
+    a2 = if x1 == 1 then 0 else m ! (x1 - 1, y2)
+    a3 = if y1 == 1 then 0 else m ! (x2, y1 - 1)
+    a4 = if x1 == 1 || y1 == 1 then 0 else m ! (x1 - 1, y1 - 1)
